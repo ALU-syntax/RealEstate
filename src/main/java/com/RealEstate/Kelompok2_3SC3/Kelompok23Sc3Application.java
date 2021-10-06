c8cb3455cc8b14ef91e8366dbd739e3e71d5e939
woodiechek <ardianiqbal40@gmail.com>
2021-10-07 02:07:38 +0700

    First Commit

---
+++ src/main/java/com/RealEstate/Kelompok2_3SC3/Kelompok23Sc3Application.java
@@ -0,0 +1,13 @@
+package com.RealEstate.Kelompok2_3SC3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kelompok23Sc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kelompok23Sc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/RealEstate/Kelompok2_3SC3/Kelompok23Sc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RealEstate.Kelompok2_3SC3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kelompok23Sc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
